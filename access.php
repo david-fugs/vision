e8032b09c6270f1b94d62f49f15f9e4acd5a047b
David Lopez <fulgore0142@gmail.com>
2024-11-27 14:00:32 -0500

    importar fontawesome en el acces

--- access.php
+++ access.php
@@ -19,6 +19,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- Boxicons CSS -->
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js" defer></script>
+
     <script src="https://kit.fontawesome.com/fed2435e21.js" crossorigin="anonymous"></script>
     <title>VISION | SOFT</title>
     <link href="fontawesome/css/all.css" rel="stylesheet">
